8f0e92ae75e573ca49a6eef92289d287d99e9b82
diogo fernando silveira de moura <diogofmouraa@gmail.com>
2023-06-26 18:08:49 -0300

    enviando a listagem

---
+++ converter.py
@@ -0,0 +1,44 @@
+def converterdolar():
+ real= float(input('digite qual valor em reais voce quer converter'))
+ converter= float(real/4.79)
+ resultado= converter
+ round(resultado,2)
+ print(resultado)
+
+def conversao()
+ dolar= 4.79
+ euro= 5.50
+ pesoArgentino= 3
+
+ if moeda == 'dolar':
+
+def calcularimc():
+ peso= input('digite seu peso')
+ altura= input('digite sua altura'
+ imc= peso/altura**2
+ return imc
+
+ def converter_moeda(valor_reais, moeda):
+  taxa_dolar = 5.30  # Exemplo de taxa de conversão de reais para dólares
+  taxa_euro = 6.20  # Exemplo de taxa de conversão de reais para euros
+  taxa_peso_argentino = 0.06  # Exemplo de taxa de conversão de reais para pesos argentinos
+
+  if moeda == "dólares":
+   valor_convertido = valor_reais / taxa_dolar
+   return valor_convertido
+  elif moeda == "euros":
+   valor_convertido = valor_reais / taxa_euro
+   return valor_convertido
+  elif moeda == "peso argentino":
+   valor_convertido = valor_reais / taxa_peso_argentino
+   return valor_convertido
+  else:
+   return "Moeda não suportada"
+
+
+
+
+
+
+
+
---
+++ menu.py
@@ -0,0 +1,5 @@
+from converter import *
+
+converterdolar()
+
+calcularimc ():
